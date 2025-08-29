457aa46c4cbebcdf2a5b38feb41480cb4ec41aea
nodedev74_25 <buelenard@tutanota.com>
2025-08-29 18:08:11 +0200

    simple tray icon

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QSystemTrayIcon>
+#include <QMenu>
+#include <QAction>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+
+    if (!QSystemTrayIcon::isSystemTrayAvailable())
+        return 1;
+
+    QMenu *menu = new QMenu();
+    QAction *quitAction = new QAction(QObject::tr("&Quit"));
+    QObject::connect(quitAction, &QAction::triggered, &app, &QCoreApplication::quit);
+    menu->addAction(quitAction);
+
+    QSystemTrayIcon *trayIcon = new QSystemTrayIcon();
+    trayIcon->setIcon(QIcon::fromTheme("application-exit"));
+    trayIcon->setContextMenu(menu);
+    trayIcon->show();
+
+    return app.exec();
 }
